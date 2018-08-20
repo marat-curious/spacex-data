bf44a58a7af187acdffdeaaf6736066ddda49992
Marat Amirov <mamirov@ubic.tech>
2018-08-20 18:54:16 +0300

    create base app

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import {h, Component} from 'preact';
+
+export interface AppProps {
+  name: string
+}
+
+interface AppState {
+  name: string
+}
+
+export class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {name: props.name};
+  }
+  render(props: AppProps, state: AppState) {
+    return <h1>props: {props.name}, state: {state.name}</h1>;
+  }
+}
